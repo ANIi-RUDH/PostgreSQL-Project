74f5ac518d6de3b503dab0272b44d5064392da90
ANI-RUDH <117719175+ANIi-RUDH@users.noreply.github.com>
2024-04-29 15:30:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "Form-1",
+  password: "jk32@12345AA",
+  port: 5432
+});
+
+db.connect();
+
+let quiz = [];
+
+db.query("SELECT * FROM capitals", (err, res) => {
+  
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;//this line fills the quiz list with the countries and capitals from the database so the random function can work
+  }
+  console.log(res.rows)
+  db.end();
+});
+
+let totalCorrect = 0;
+
+
+let currentQuestion = {};
+
+// GET home page
+app.get("/", async (req, res) => {
+  totalCorrect = 0;
+  await nextQuestion();
+  console.log(currentQuestion);
+  res.render("index.ejs", { question: currentQuestion });
+});
+
+// POST a new post
+app.post("/submit", (req, res) => {
+  let answer = req.body.answer.trim();
+  let isCorrect = false;
+  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+    totalCorrect++;
+    console.log(totalCorrect);
+    isCorrect = true;
+  }
+
+  nextQuestion();
+  res.render("index.ejs", {
+    question: currentQuestion,
+    wasCorrect:isCorrect,
+    totalScore: totalCorrect,
+  });
+});
+
+async function nextQuestion() {
+  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+
+  currentQuestion = randomCountry;
+}
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
